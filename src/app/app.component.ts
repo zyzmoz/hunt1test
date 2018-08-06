b61f635c211795326345671fb147e19efca8193c
HackerRank <hackerrank@example.com>
2018-08-06 13:14:09 +0000

    Saved at Mon Aug 06 2018 10:14:09 GMT-0300 (Horário Padrão de Brasília)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,9 +27,9 @@ export class AppComponent {
   ngOnInit() {
      this.paymentForm = this.formBuilder.group({
         cHolder: ['', Validators.required, Validators.pattern(/^[a-zA-Z]+/)],
-        cNumber: ['', Validators.required, Validators.minLength(16), Validators.maxLength(16), Validators.pattern(/^[0-9]+/)],
+        cNumber: ['', Validators.required, Validators.minLength(16), Validators.maxLength(16), Validators.pattern(/^-?(0|[1-9]\d*)?$/)],
         exMonth: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2), Validators.pattern(/^-?(0|[1-9]\d*)?$/)],
-        exYear: ['', [Validators.required, Validators.minLength(6)]],
+        exYear: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2), Validators.pattern(/^-?(0|[1-9]\d*)?$/)],
         cvvCvc: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
