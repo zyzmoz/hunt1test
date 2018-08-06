3d421dc0c5089d8b50b0387e26b6e1f19b45bbee
HackerRank <hackerrank@example.com>
2018-08-06 12:53:44 +0000

    Saved at Mon Aug 06 2018 09:53:45 GMT-0300 (Horário Padrão de Brasília)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {AppComponent} from './app.component';
 
 @NgModule({
   /* set respective calls to components, services, etc here */
